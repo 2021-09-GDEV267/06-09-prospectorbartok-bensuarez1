283456ebbcc581a731cacc3d4a71699102c910f5
bensuarez1 <G00292013@stu.raritanval.edu>
2021-11-04 12:10:08 -0400

    Implemented these scripts: Bartok, CardBartok & BartokLayout

---
+++ Assets/Prospector/__Scripts/BartokLayout.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SlotDef
+{
+    public float x;
+    public float y;
+    public bool faceUp = false;
+    public string layerName = "Default";
+    public int layerID = 0;
+    public int id;
+    public List<int> hiddenBy = new List<int>();
+    public float rot;
+    public string type = "slot";
+    public Vector2 stagger;
+    public int player;
+    public Vector3 pos;
+}
+
+public class BartokLayout : MonoBehaviour
+{
+    
+}
--- Assets/Prospector/__Scripts/Utils.cs
+++ Assets/Prospector/__Scripts/Utils.cs
@@ -10,10 +10,29 @@ public enum BoundsTest {
 }
 
 public class Utils : MonoBehaviour {
-	
-	
-//============================ Bounds Functions ============================\
-	
+
+
+	//============================ Bounds Functions ============================\
+
+	static public Quaternion Bezier(float u, List<Quaternion> vList)
+	{
+		// If there is only one element in vList, return it
+		if (vList.Count == 1)
+		{
+			return (vList[0]);
+		}
+		// Otherwise, create vListR, which is all but the 0th element of vList
+		// e.g. if vList = [0,1,2,3,4] then vListR = [1,2,3,4]
+		List<Quaternion> vListR = vList.GetRange(1, vList.Count - 1);
+		// And create vListL, which is all but the last element of vList
+		// e.g. if vList = [0,1,2,3,4] then vListL = [0,1,2,3]
+		List<Quaternion> vListL = vList.GetRange(0, vList.Count - 1);
+		// The result is the Slerp of these two shorter Lists
+		// It's possible that Quaternion.Slerp may clamp u to [0..1] :(
+		Quaternion res = Quaternion.Slerp(Bezier(u, vListL), Bezier(u, vListR), u);
+		return (res);
+	}
+
 	// Creates bounds that encapsulate of the two Bounds passed in.
 	public static Bounds BoundsUnion( Bounds b0, Bounds b1 ) {
 		// If the size of one of the bounds is Vector3.zero, ignore that one
